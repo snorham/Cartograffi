f8855688a9230473fc28e90261ca8f1e44a77e39
Brandy Foster <brandy.foster@detroitlabs.com>
2015-01-07 19:37:20 -0500

    created dialog frag class.

---
+++ app/src/main/java/com/detroitlabs/cartograffi/fragments/DeleteConfirmationDialogFragment.java
@@ -0,0 +1,16 @@
+package com.detroitlabs.cartograffi.fragments;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+/**
+ * Created by BFineRocks on 1/7/15.
+ */
+public class DeleteConfirmationDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        
+    }
+}
