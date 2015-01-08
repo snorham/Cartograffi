bd3c1b72f39538962d32ab891f71f57e0a2bb82f
Brandy Foster <brandy.foster@detroitlabs.com>
2015-01-07 19:44:37 -0500

    added interface and new instance method

--- app/src/main/java/com/detroitlabs/cartograffi/fragments/DeleteConfirmationDialogFragment.java
+++ app/src/main/java/com/detroitlabs/cartograffi/fragments/DeleteConfirmationDialogFragment.java
@@ -1,16 +1,48 @@
 package com.detroitlabs.cartograffi.fragments;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+
+import com.detroitlabs.cartograffi.R;
 
 /**
  * Created by BFineRocks on 1/7/15.
  */
 public class DeleteConfirmationDialogFragment extends DialogFragment {
 
+    public interface DeleteConfirmationInterface {
+        public void onDialogClick(boolean confirmationClick);
+    }
+
+    private DeleteConfirmationInterface confirmationInterface;
+
+    public static DeleteConfirmationDialogFragment newInstance(DeleteConfirmationInterface deleteConfirmationInterface){
+        DeleteConfirmationDialogFragment deleteDialog = new DeleteConfirmationDialogFragment();
+        deleteDialog.confirmationInterface = deleteConfirmationInterface;
+        return deleteDialog;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        
+        final AlertDialog.Builder deleteConfirmation = new AlertDialog.Builder(getActivity());
+        deleteConfirmation.setTitle(R.string.view_detail_delete_confirm_title);
+        deleteConfirmation.setMessage(R.string.view_detail_delete_confirm_message);
+        deleteConfirmation.setPositiveButton(R.string.view_detail_delete_confirm_positive, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        deleteConfirmation.setNegativeButton(R.string.view_detail_delete_confirm_negative, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // dialog is dismisssed
+            }
+        });
+        return deleteConfirmation.create();
     }
 }
